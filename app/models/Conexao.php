116b4a54427e4da47fb8b1b56cc383de12e0cc1c
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2017-11-23 13:41:58 -0200

    Update Conexao.php

--- app/models/Conexao.php
+++ app/models/Conexao.php
@@ -14,7 +14,13 @@ class Conexao {
     const SENHA     = "?";
 
     public static function getConexao(){
-        $conexao = new PDO("mysql:host=".self::HOST.";dbname=".self::NOMEBANCO, self::USUARIO, self::SENHA);
+        
+        try{
+            $conexao = new PDO("mysql:host=".self::HOST.";dbname=".self::NOMEBANCO, self::USUARIO, self::SENHA);
+            $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die("Falhou a conexao ou ocorreu um erro banco: " . $e->getMessage()); 
+        }
 
         return $conexao;
     }
